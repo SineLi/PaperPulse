2f1cdf776e4c1da62f8580e3441ac2cbd9219156
Sine <lijuzhe@outlook.com>
2026-01-05 11:08:51 +0800

    新增ACS抓取器

---
+++ utils/fetcher/ACS_fetcher.py
@@ -0,0 +1,49 @@
+import html
+from lxml import html as lhtml
+from utils.fetcher.Base_fetcher import RSSFetcher, Dict
+
+DEFAULT_FEED_URL = "https://pubs.acs.org/action/showFeed?type=axatoc&feed=rss&jc=jacsat"
+class RSCFetcher(RSSFetcher):
+    def __init__(self):
+        super().__init__(
+            journal_name="Journal of the American Chemical Society", 
+            feed_url=DEFAULT_FEED_URL,
+            max_workers=8,
+            max_pages=0
+        )
+
+    def _parse_entry(self, entry) -> Dict:
+        return {
+            'title': entry.get('title', 'No Title').replace("[ASAP] ","").strip(),
+            'link': entry.get('link', ''),
+            'date': self._parse_date(entry),
+            'journal': self.journal_name,
+            'authors': self._extract_authors(entry),
+            'doi': self._extract_doi(entry),
+            'status': 'online'
+        }
+
+    def fetch_details(self, article):
+        link = article.get('link')
+        
+        content = self._get_playwright_content(link, wait_until="domcontentloaded")
+        if not content: return {}
+
+        tree = lhtml.fromstring(content)
+        info = {}
+
+        # Abstract
+        abstract_parts = tree.xpath('//div[@id="abstractBox"]/p//text()')
+        if abstract_parts:
+            info['abstract'] = html.unescape(' '.join([p.strip() for p in abstract_parts if p.strip()]))
+
+        # graphical abstract
+        ga_parts = tree.xpath('//figure[starts-with(@id, "_i")]//img/@src')
+        if ga_parts:
+            info['graphical_abstract'] = "https://pubs.rsc.org" + ga_parts[0]
+
+        return info
+
+if __name__ == "__main__":
+    RSCFetcher().run()
+
