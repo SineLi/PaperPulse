1d02af3cead07335efaddaa5cfa62afa8c0031ca
Sine <lijuzhe@outlook.com>
2026-01-06 18:14:06 +0800

    修改为通过API获取信息，避免防爬拦截

--- utils/fetcher/Elsevier_fetcher.py
+++ utils/fetcher/Elsevier_fetcher.py
@@ -1,60 +1,95 @@
 import html
-from lxml import html as lhtml
+from lxml import etree
 from utils.fetcher.Base_fetcher import RSSFetcher, Dict
+import requests
+
+from API_KEYs import Elsevier_KEY
 
 DEFAULT_FEED_URL = "https://rss.sciencedirect.com/publication/science/03088146"
+API_ADDRESS = 'https://api.elsevier.com/content/article/pii/'
+
+ns = {
+    'def': 'http://www.elsevier.com/xml/svapi/article/dtd',
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'prism': 'http://prismstandard.org/namespaces/basic/2.0/',
+    'ce': 'http://www.elsevier.com/xml/common/dtd',
+    'xocs': 'http://www.elsevier.com/xml/xocs/dtd',
+    'dcterms': 'http://purl.org/dc/terms/'
+}
+
+
 class ElsevierFetcher(RSSFetcher):
     def __init__(self):
         super().__init__(
             journal_name="Food Chemistry", 
             feed_url=DEFAULT_FEED_URL,
-            max_workers=8,
-            max_pages=0
+            max_workers=4,
+            max_pages=0,
+            sleep_time=1,
         )
 
     def _parse_entry(self, entry) -> Dict:
         return {
             'title': entry.get('title', 'No Title'),
-            'link': entry.get('link', ''),
+            'link': entry.get('link', '').replace("?dgcid=rss_sd_all", ""),
             'journal': self.journal_name,
-            'date' : 
-            lhtml.fromstring(entry.get('summary', '')).xpath('//p[1]/text()[contains(., "Publication date:")]')[0].split("Publication date:")[-1].strip() if "Publication date:" in entry.get('summary', "") else "",
             'status': 'online'
         }
 
+    def _get_ga_url(self, pii):
+        try:
+            for gav in range(1,4):
+                for resv in [2,1,3]:
+                    for size in ['lrg','']:
+                        url = f"https://ars.els-cdn.com/content/image/1-s{resv}.0-{pii}-ga{gav}_{size}.jpg"
+                        response = requests.head(url, timeout=5)
+                        if response.status_code == 200:
+                            return url
+                        else:
+                            continue
+        except Exception as e:
+            print(f"Error fetching graphical abstract for PII {pii}: {e}")
+
     def fetch_details(self, article):
         link = article.get('link')
-        
-        content = self._get_playwright_content(link, wait_until="domcontentloaded")
-        if not content: return {}
+        pii = link.split("/")[-1].split("?")[0]  # 提取 PII
+        url = API_ADDRESS + "{" + pii + "}"
+        headers = {
+            'XSD': 'text/xml',
+            'X-ELS-APIKey': Elsevier_KEY,
+            'Accept': 'text/xml'
+        }
 
-        tree = lhtml.fromstring(content)
+        response = requests.get(url, headers=headers, timeout=15)
+        if response.status_code != 200:
+            print(f"Failed to fetch details for {link}: {response.status_code}")
+            return {}
+
+        tree = etree.fromstring(response.content)
         info = {}
         # DOI
-        doi_parts = tree.xpath('//a[contains(@class,"doi")]/@href')
+        doi_parts = tree.xpath('//prism:doi/text()', namespaces=ns)
         if doi_parts:
             info['doi'] = doi_parts[0].replace("https://doi.org/","").strip()
 
         # Authors
-        authors_parts = tree.xpath('//div[@id="author-group"]//button//text()[not(ancestor::sup)]')
+        authors_parts = tree.xpath('//dc:creator/text()', namespaces=ns)
         authors = [a.strip() for a in authors_parts if a.strip()]
         if authors:
             info['authors'] = authors
 
-        # editorial summary
-        ed_summary_parts = tree.xpath('//div[@class="abstract author-highlights"]//text()')
-        if ed_summary_parts:
-            info['editor_summary'] = html.unescape(' '.join([p.strip() for p in ed_summary_parts if p.strip()]))
+        # date
+        date_parts = tree.xpath('//prism:coverDate/text()', namespaces=ns)
+        if date_parts:
+            info['date'] = date_parts[0].strip()
 
         # Abstract
-        abstract_parts = tree.xpath('//div[@class="abstract author"]/p//text()[not(ancestor::h2)]')
+        abstract_parts = tree.xpath('//dc:description/text()', namespaces=ns)
         if abstract_parts:
             info['abstract'] = html.unescape(' '.join([p.strip() for p in abstract_parts if p.strip()]))
 
         # graphical abstract
-        ga_parts = tree.xpath('//div[@class="abstract graphical"]//ol/li[1]/a/@href') if tree.xpath('//div[@class="abstract graphical"]//ol/li[1]/a/@href') else tree.xpath('//div[@class="abstract graphical"]//img/@src')
-        if ga_parts:
-            info['graphical_abstract'] = ga_parts[0]
+        info['graphical_abstract'] = self._get_ga_url(pii)
 
         return info
 
