991260a789667f4013c004c3656030a231f68622
Sine <lijuzhe@outlook.com>
2026-01-10 22:35:04 +0800

    加入batch任务上传

--- services/LLM_service.py
+++ services/LLM_service.py
@@ -1,5 +1,7 @@
 from db.database import get_db_connection
 import time
+import json
+import io
 from openai import OpenAI
 from API_KEYs import LM_API_KEY
 
@@ -27,7 +29,64 @@ class LLMService:
             return abstracts
 
     def build_batch(self, abstracts):
+        """
+        {
+        "custom_id": "1",
+        "method": "POST",
+        "url": "/v1/chat/ds-test",
+        "body": {
+            "model": "batch-test-model",
+            "messages": [
+            { "role": "system", "content": "You are a helpful assistant." },
+            { "role": "user", "content": "你好！有什么可以帮助你的吗？" }
+            ]
+        }
+        }
+        """
+        jsons = []
+        for i, abstract in enumerate(abstracts[0:10]):
+            entry = {
+                "custom_id": f"request-{i}",
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": "qwen-plus", 
+                    "messages": [
+                        {"role": "user", "content": abstract}
+                    ],
+                }
+            }
+            jsons.append(json.dumps(entry, ensure_ascii=False))
+        
+        # 将列表转换为换行符分隔的字符串，并转换为字节流
+        jsonl_content = "\n".join(jsons).encode("utf-8")
+        file_io = io.BytesIO(jsonl_content)
 
+        file_id = self.app.files.create(
+            file=("batch_input.jsonl", file_io),
+            purpose="batch"
+        ).id
+        
+        batch_id = self.app.batches.create(
+            input_file_id=file_id, 
+            endpoint="/v1/chat/completions", 
+            completion_window="24h"
+        ).id
+
+        result_id = None
+        while True:
+            time.sleep(20)
+            batch = self.app.batches.retrieve(batch_id)
+            if batch.status == "completed":
+                result_id = batch.output_file_id
+                print(result_id)
+                break
+            elif batch.status == "in_progress":
+                print("Batch is still processing...")
+                continue
+            elif batch.status == "failed":
+                result_id = batch.error_file_id
+                raise Exception("Batch processing failed")
 
     def generate_summary(self, texts):
         pass
