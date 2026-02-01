79665a4cd8df742090936dee7a529af989b7173e
Sine <lijuzhe@outlook.com>
2026-02-01 10:42:13 +0800

    初始化依赖

--- frontend_app/lib/main.dart
+++ frontend_app/lib/main.dart
@@ -1,18 +1,27 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'data/api/client.dart';
 import 'data/auth/auth_services.dart';
 import 'data/auth/auth_storage.dart';
-import 'data/api/client.dart';
+import 'data/db/articledb.dart';
+import 'data/db/subscripdb.dart';
+import 'data/db/syncdb.dart';
+import 'data/repositories/feed_repo.dart';
+import 'data/repositories/user_repo.dart';
+import 'data/service/feed_service.dart';
+import 'data/service/sync_service.dart';
 import 'data/service/user_services.dart';
 
 Future<void> main() async {
-  print("Starting app...");
   WidgetsFlutterBinding.ensureInitialized();
 
+  final authStorage = AuthStorage();
   final apiClient = ApiClient(
     baseUrl: 'http://10.0.2.2:8000',
-    authStorage: AuthStorage(),
+    authStorage: authStorage,
   );
-  final authStorage = AuthStorage();
+
   final userServices = UserServices(apiClient: apiClient);
   final authServices = AuthServices(
     apiClient: apiClient,
@@ -20,17 +29,26 @@ Future<void> main() async {
     userServices: userServices,
   );
 
-  final user = await authServices.tryGetCurrentUser();
-  if (user == null) {
-    print("No authenticated user.");
-  } else {
-    print("Current user: ${user.username}");
-  }
-
-  runApp(
-    const Directionality(
-      textDirection: TextDirection.ltr,
-      child: Placeholder(),
-    ),
+  final articleDb = ArticleDatabaseIO();
+  final feedService = FeedService(apiClient: apiClient);
+  final feedRepo = FeedRepo(
+    feedService: feedService,
+    articleDatabaseIO: articleDb,
   );
+
+  final syncDb = SyncDatabaseIO();
+  final syncService = SyncService(
+    apiClient: apiClient,
+    syncDatabase: syncDb,
+    articleDatabase: articleDb,
+  );
+
+  final subscriptionDb = SubscriptionDatabaseIO();
+  final userRepo = UserRepo(
+    userServices: userServices,
+    subscriptionDatabaseIO: subscriptionDb,
+    feedRepo: feedRepo,
+  );
+
+  runApp(Placeholder());
 }
